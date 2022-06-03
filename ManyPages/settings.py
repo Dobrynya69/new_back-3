0e3f517b9591852530f8658aafe561fa2338b7a1
Roman <roma040805@gmail.com>
2022-06-04 01:07:04 +0300

    Setting for my first deploy. Heroku

--- ManyPages/settings.py
+++ ManyPages/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g0pkjw$ofz2dz_a(u(@wm%1lc_u3)#_*t*vvxm1n%#zukg^dv@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
